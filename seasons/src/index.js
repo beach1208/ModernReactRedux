df82ca72b77f72151bd785b424de4d60c39d0005
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-03-08 17:37:33 -0800

    state object added and changed value

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,13 +3,40 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+
+    //we can define another function
+    //when we make constructor, props is called automatically
+    //super(props) means constructor function from React.Component is called
+    constructor(props) {
+        super(props);
+
+        //set default value of latitude. if you dont know default, set null
+        this.state = {lat: null, errorMessage: ''};
+
+        //get user location function
+        //if its in render() method, it will be called over and over when its redered.
+        window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+            this.setState({lat: position.coords.latitude});
+        },
+        (err) => {
+            this.setState({errorMessage: err.message});
+        }
+        );
+        
+    }
+
+    //React says we have to define render!
     render() {
-         //get user location function
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-        return <div>Latitude: </div>
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>;
     }
 }
 
