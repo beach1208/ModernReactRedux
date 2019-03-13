4cd53f5e30ec861a4eb407e60a1896e0e6ede7b8
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-03-13 00:25:03 -0700

    Season Display logics added

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
-    //we can define another function
-    //when we make constructor, props is called automatically
-    //super(props) means constructor function from React.Component is called
-    constructor(props) {
-        super(props);
+    state = {lat: null, errorMessage: ''}
 
-        //set default value of latitude. if you dont know default, set null
-        this.state = {lat: null, errorMessage: ''};
-
-        //Data loading inside constructor is not recommended from official document
-        //get user location function
-        //if its in render() method, it will be called over and over when its redered.
+    //Data loading inside constructor is not recommended from official document,
+    //so better to make componentDidMount() and load deta inside.
+    //get user location function
+    //if its in render() method, it will be called over and over when its redered.
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-            this.setState({lat: position.coords.latitude});
-        },
-        (err) => {
-            this.setState({errorMessage: err.message});
-        }
+        (position) => this.setState({lat: position.coords.latitude}),
+        (err) => this.setState({errorMessage: err.message})
         );
-        
     }
 
     //React says we have to define render!
@@ -34,7 +24,8 @@ class App extends React.Component {
         }
 
         if(!this.state.errorMessage && this.state.lat){
-            return <div>latitude: {this.state.lat}</div>
+            //pass state property as props
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
         return <div>Loading!</div>;
