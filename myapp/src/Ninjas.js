b5e214244ed861d95b5a81e48793e917ce231dfb
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-05-26 23:29:13 -0700

    form app added

---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+const Ninjas = (props) => {
+    // when you make a functional component, you have to pass props as a parameter
+    // when its class component props is already passed from parent component 
+    // const {ninjas} = this.props works.
+    // functional component doesnt require state and you cant use state
+     const { ninjas } = props;
+        const ninjaList = ninjas.map(ninja => {
+            return ninja.age > 20 ? (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name:{ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                </div>
+            ) : null;
+        })
+        return(
+            <div className="ninja-list">
+             {ninjaList}
+            </div>
+        )
+}
+ 
+
+export default Ninjas
