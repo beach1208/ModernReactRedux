ef6446f3cf0e9e0c46efd7876645f197f799a2ad
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-05-27 22:59:52 -0700

    form app added

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -5,13 +5,15 @@ const Ninjas = (props) => {
     // when its class component props is already passed from parent component 
     // const {ninjas} = this.props works.
     // functional component doesnt require state and you cant use state
-     const { ninjas } = props;
+     const { ninjas, deleteNinja } = props;
         const ninjaList = ninjas.map(ninja => {
             return ninja.age > 20 ? (
                 <div className="ninja" key={ninja.id}>
                     <div>Name:{ninja.name}</div>
                     <div>Age: {ninja.age}</div>
                     <div>Belt: {ninja.belt}</div>
+                    {/* Anonymous function. its gonna be invoked with only click event */}
+                    <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>
                 </div>
             ) : null;
         })
