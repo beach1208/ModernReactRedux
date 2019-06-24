8224aeb2183ffd6c2c1a8365bf7b8527d44e92fe
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-06-23 17:39:46 -0700

    redux-thunk added

---
+++ blog/src/actions/index.js
@@ -0,0 +1,14 @@
+import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+
+// Define a function returning a dispatch function
+export const fetchPosts = () => async dispatch => {
+        const response = await jsonPlaceHolder.get('/posts');
+
+        dispatch( { type: 'FETCH_POSTS', payload: response })
+    
+};
+
+// redux thunk returns pure objects or function
+// call dispatch function and pass action Objects
+// add async function. Async function used to return requested objects instead of action object.
+// With Redux thunk, it modifies to action objects.
